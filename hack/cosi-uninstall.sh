a437b224a6bc3973b5408deaf81a5581554c9d90
Akash Mukhopadhyay <akmukhop@microsoft.com>
2022-11-10 09:41:06 -0800

    Fixing shellcheck errors

--- hack/cosi-uninstall.sh
+++ hack/cosi-uninstall.sh
@@ -1,16 +1,17 @@
+#!/bin/bash
 echo -e "\nDeleting Kube Resources"
 kubectl delete -k ./. 
 echo -e "\n"
 
 echo -e "\nDeleting COSI Driver and Sidecar"
-DRIVER_NAME=$(dirname $(dirname "$(realpath ${BASH_SOURCE[0]})"))
-cd $DRIVER_NAME
-echo $(pwd)
+DRIVER_NAME=$(dirname "$(dirname "$(realpath "${BASH_SOURCE[0]}")")")
+cd $DRIVER_NAME || echo "Unable to find directory $DRIVER_NAME"; exit 1
+echo "$(pwd)"
 
 echo -e "\nDeleting COSI Image"
 PROPERTY_FILE="./resources/cosi-driver-azure.properties"
-export REGISTRY=`cat $PROPERTY_FILE | grep AZURE_DRIVER_IMAGE_ORG | cut -d'=' -f2`
-export IMAGE_VERSION=`cat $PROPERTY_FILE | grep AZURE_DRIVER_IMAGE_VERSION | cut -d'=' -f2`
+export REGISTRY=`cat "${PROPERTY_FILE}" | grep AZURE_DRIVER_IMAGE_ORG | cut -d'=' -f2`
+export IMAGE_VERSION=`cat "${PROPERTY_FILE}" | grep AZURE_DRIVER_IMAGE_VERSION | cut -d'=' -f2`
 echo "Registry: $REGISTRY"
 echo "Version: $IMAGE_VERSION"
 docker image rm "$REGISTRY/azure-cosi-driver:$IMAGE_VERSION"
